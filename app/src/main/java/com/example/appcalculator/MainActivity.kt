691e778af86b834fded95ae164209f6535400a39
conganh11 <phungconganh99prott@gmail.com>
2021-08-19 20:29:21 +0700

    fix

--- app/src/main/java/com/example/appcalculator/MainActivity.kt
+++ app/src/main/java/com/example/appcalculator/MainActivity.kt
@@ -23,11 +23,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
     private fun initViews() {
         with(binding) {
-            numberTextViews = listOf(tvOne, tvTwo, tvThree, tvFour, tvFive, tvSix, tvSeven, tvEight, tvNine, tvZero, tvDot)
-            actionTextViews = listOf(tvPlus,tvSub, tvMulti, tvDiv)
-            tvClear.setOnClickListener(this@MainActivity)
-            tvEquals.setOnClickListener(this@MainActivity)
-            tvBack.setOnClickListener(this@MainActivity)
+            numberTextViews = listOf(textOne, textTwo, textThree, textFour, textFive, textSix, textSeven, textEight, textNine, textZero, textDot)
+            actionTextViews = listOf(textPlus, textSub, textMulti, textDiv)
+            textClear.setOnClickListener(this@MainActivity)
+            textEquals.setOnClickListener(this@MainActivity)
+            textBack.setOnClickListener(this@MainActivity)
         }
         numberTextViews.forEach { it.setOnClickListener(this) }
         actionTextViews.forEach { it.setOnClickListener(this) }
@@ -35,55 +35,55 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) = with(binding) {
         when (v){
-            tvOne -> showExpression(getString(R.string.text_1))
-            tvTwo -> showExpression(getString(R.string.text_2))
-            tvThree -> showExpression(getString(R.string.text_3))
-            tvFour -> showExpression(getString(R.string.text_4))
-            tvFive -> showExpression(getString(R.string.text_5))
-            tvSix -> showExpression(getString(R.string.text_6))
-            tvSeven -> showExpression(getString(R.string.text_7))
-            tvEight -> showExpression(getString(R.string.text_8))
-            tvNine -> showExpression(getString(R.string.text_9))
-            tvZero -> showExpression(getString(R.string.text_0))
-            tvDot -> showExpression(getString(R.string.text_dot))
+            textOne -> showExpression(getString(R.string.text_1))
+            textTwo -> showExpression(getString(R.string.text_2))
+            textThree -> showExpression(getString(R.string.text_3))
+            textFour -> showExpression(getString(R.string.text_4))
+            textFive -> showExpression(getString(R.string.text_5))
+            textSix -> showExpression(getString(R.string.text_6))
+            textSeven -> showExpression(getString(R.string.text_7))
+            textEight -> showExpression(getString(R.string.text_8))
+            textNine -> showExpression(getString(R.string.text_9))
+            textZero -> showExpression(getString(R.string.text_0))
+            textDot -> showExpression(getString(R.string.text_dot))
 
-            tvPlus -> showExpression(getString(R.string.text_plus))
-            tvSub -> showExpression(getString(R.string.text_sub))
-            tvMulti -> showExpression(getString(R.string.text_mul))
-            tvDiv -> showExpression(getString(R.string.text_div))
+            textPlus -> showExpression(getString(R.string.text_plus))
+            textSub -> showExpression(getString(R.string.text_sub))
+            textMulti -> showExpression(getString(R.string.text_mul))
+            textDiv -> showExpression(getString(R.string.text_div))
 
-            tvClear -> clearExpression()
-            tvBack -> backExpression()
-
-            tvEquals -> showResult()
+            textClear -> clearExpression()
+            textBack -> backExpression()
+            textEquals -> showResult()
         }
     }
 
     private fun showExpression(string: String){
         with(binding){
-            tvDisplay.append(string)
+            textDisplay.append(string)
         }
     }
 
     private fun clearExpression(){
         with(binding){
-            tvDisplay.text = getString(R.string.text_emp)
+            textDisplay.text = ""
         }
     }
     private fun backExpression(){
         with(binding){
-            val expression = tvDisplay.text.toString()
+            val expression = textDisplay.text.toString()
             if (expression.isNotEmpty()){
-                tvDisplay.text = expression.substring(0, expression.length - 1)
+                textDisplay.text = expression.substring(0, expression.length - 1)
             }
 
         }
     }
     private fun showResult(){
         with(binding){
-            val expression = ExpressionBuilder(tvDisplay.text.toString()).build()
+            val expression = ExpressionBuilder(textDisplay.text.toString()).build()
             val result = expression.evaluate()
-            tvDisplay.text = result.toString()
+
+            textDisplay.text = result.toString()
 
         }
     }
